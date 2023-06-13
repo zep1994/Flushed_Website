5f35ff77053681745afa6d6dfdb688d66e5ce535
Thomas Matlock <thomasmatlockbba@gmail.com>
2023-06-13 11:07:10 -0500

    Minor changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,75 +1,129 @@
-import Image from 'next/image'
-import styles from './styles/Home.module.css'
+import styles from "./styles/Home.module.css";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-       {/* SECTION 1 */}
-       <div className={styles.section1}>
-            <div className={styles.introContent}>
-              <p className={styles.hi}>Welcome To Flushed</p>
-              <h1 className={styles.introHeader}>A healthier you</h1>
-              <h1 className={styles.secondaryHeader}>
-                One Flush At a Time
-              </h1>
+      {/* SECTION 1 */}
+      <div className={styles.section1}>
+        <div className={styles.introContent}>
+          <p className={styles.hi}>Welcome To Flushed</p>
+          <h1 className={styles.introHeader}>A healthier you</h1>
+          <h1 className={styles.secondaryHeader}>One Flush At a Time</h1>
+          <p className={styles.description}>
+            Welcome to Flushed: Your Comprehensive Guide to IBS Management and
+            Technology Integration! Flushed is a user-friendly website designed
+            to provide valuable information and practical resources for
+            individuals living with Irritable Bowel Syndrome (IBS). Our mission
+            is to empower IBS patients by equipping them with knowledge about
+            the condition, effective treatment strategies, and innovative
+            technological solutions that can enhance their quality of life. At
+            Flushed, we understand that IBS can be a challenging condition to
+            navigate. Thats why our website offers a clear and concise overview
+            of IBS, explaining its causes, symptoms, and impact on daily life.
+            Through easy-to-understand language and relatable examples, we aim
+            to demystify this complex gastrointestinal disorder and help
+            visitors develop a deeper understanding of their own experiences.
+            Finding effective treatment options for IBS is a priority for us. On
+            Flushed, we provide a wealth of evidence-based resources to guide
+            individuals in their journey toward symptom relief. From dietary
+            recommendations and stress management techniques to lifestyle
+            modifications and alternative therapies, our comprehensive treatment
+            section offers a range of strategies tailored to address the diverse
+            needs of IBS patients. But thats not all! We recognize the
+            potential of technology to revolutionize IBS management, and our
+            website delves into the integration of technology for improved
+            quality of life. We showcase cutting-edge tools such as symptom
+            tracking apps, virtual support networks, and telemedicine options
+            that can empower individuals to monitor their symptoms, connect with
+            a supportive community, and access healthcare professionals from the
+            comfort of their homes. Flushed is a hub of curated articles, expert
+            insights, and personal stories from individuals who have
+            successfully integrated technology into their IBS management. We
+            strive to provide the most up-to-date information on technological
+            advancements, ensuring that visitors are at the forefront of
+            innovation in IBS care. Whether youre newly diagnosed with IBS, a
+            long-time sufferer seeking new treatment avenues, or a healthcare
+            professional looking to stay informed, Flushed is your go-to
+            resource. We believe that by combining knowledge, effective
+            strategies, and technology, individuals can regain control over
+            their lives and enjoy a better quality of life with IBS. Join us at
+            Flushed today and embark on a journey toward empowered IBS
+            management and technological transformation. Your well-being
+            matters, and were here to support you every step of the way!
+          </p>
+        </div>
+      </div>
+
+      {/* SECTION 2 */}
+      <div id="about" className={styles.section2}>
+        <div className={styles.content2}>
+          <div className={styles.info2}>
+            <div className={styles.txtBox}>
+              <div className={styles.txtHeads}>
+                <p className={styles.sec1}>01.</p>
+                <p className={styles.about}>About Me</p>
+              </div>
+              <br />
+              <br />
               <p className={styles.description}>
-              Welcome to Flushed: Your Comprehensive Guide to IBS Management and Technology Integration!
-
-Flushed is a user-friendly website designed to provide valuable information and practical resources for individuals living with Irritable Bowel Syndrome (IBS). Our mission is to empower IBS patients by equipping them with knowledge about the condition, effective treatment strategies, and innovative technological solutions that can enhance their quality of life.
-
-At Flushed, we understand that IBS can be a challenging condition to navigate. That's why our website offers a clear and concise overview of IBS, explaining its causes, symptoms, and impact on daily life. Through easy-to-understand language and relatable examples, we aim to demystify this complex gastrointestinal disorder and help visitors develop a deeper understanding of their own experiences.
-
-Finding effective treatment options for IBS is a priority for us. On Flushed, we provide a wealth of evidence-based resources to guide individuals in their journey toward symptom relief. From dietary recommendations and stress management techniques to lifestyle modifications and alternative therapies, our comprehensive treatment section offers a range of strategies tailored to address the diverse needs of IBS patients.
-
-But that's not all! We recognize the potential of technology to revolutionize IBS management, and our website delves into the integration of technology for improved quality of life. We showcase cutting-edge tools such as symptom tracking apps, virtual support networks, and telemedicine options that can empower individuals to monitor their symptoms, connect with a supportive community, and access healthcare professionals from the comfort of their homes.
-
-Flushed is a hub of curated articles, expert insights, and personal stories from individuals who have successfully integrated technology into their IBS management. We strive to provide the most up-to-date information on technological advancements, ensuring that visitors are at the forefront of innovation in IBS care.
-
-Whether you're newly diagnosed with IBS, a long-time sufferer seeking new treatment avenues, or a healthcare professional looking to stay informed, Flushed is your go-to resource. We believe that by combining knowledge, effective strategies, and technology, individuals can regain control over their lives and enjoy a better quality of life with IBS.
-
-Join us at Flushed today and embark on a journey toward empowered IBS management and technological transformation. Your well-being matters, and we're here to support you every step of the way!
+                Discover Effective Treatments for Irritable Bowel Syndrome (IBS)
+                at Flushed! At Flushed, we understand the significant impact
+                that Irritable Bowel Syndrome (IBS) can have on your daily life.
+                Thats why our website is dedicated to providing a comprehensive
+                guide to different types of treatments available to manage IBS
+                symptoms effectively. Whether youre newly diagnosed or have
+                been living with IBS for years, we are here to help you find
+                relief and improve your quality of life. Our website covers a
+                wide range of treatment options, tailored to address the unique
+                needs and preferences of individuals with IBS. We believe in a
+                holistic approach to IBS management, taking into account the
+                interconnectedness of the mind and body. Here are some of the
+                key treatment strategies we explore: Dietary Modifications:
+                Flushed delves into the crucial role that diet plays in managing
+                IBS symptoms. We provide in-depth information on various dietary
+                approaches, including the low FODMAP diet, which can help
+                identify and eliminate trigger foods that may exacerbate IBS
+                symptoms. Our articles and resources offer practical guidance
+                and tips for implementing dietary changes effectively. Lifestyle
+                Adjustments: We recognize that making certain lifestyle
+                adjustments can significantly impact IBS symptoms. Our website
+                offers insights into stress management techniques, regular
+                exercise routines, sleep hygiene practices, and mindfulness
+                exercises that can help alleviate IBS-related stress and promote
+                overall well-being. Medications: Flushed provides an overview of
+                commonly prescribed medications for IBS, such as antispasmodics,
+                antidiarrheals, and laxatives. We explain how these medications
+                work, their potential side effects, and when they may be
+                recommended as part of an individualized treatment plan. Its
+                important to note that medication should always be discussed
+                with a healthcare professional. Alternative Therapies: We
+                explore a variety of alternative therapies that have shown
+                promise in managing IBS symptoms. From acupuncture and herbal
+                remedies to probiotics and gut-directed hypnotherapy, Flushed
+                presents evidence-based insights on these complementary
+                approaches, helping you make informed decisions about
+                incorporating them into your treatment regimen. Psychological
+                Support: Recognizing the strong connection between the gut and
+                the brain, our website emphasizes the importance of
+                psychological support in IBS management. We provide information
+                on cognitive behavioral therapy (CBT), relaxation techniques,
+                and support groups that can help individuals cope with the
+                emotional and psychological aspects of living with IBS. At
+                Flushed, we strive to empower individuals with knowledge,
+                resources, and support on their journey to finding effective
+                treatment options for IBS. Our website is designed to be a
+                reliable and user-friendly platform, offering a wealth of
+                curated articles, expert advice, and real-life success stories
+                from those who have found relief. If youre seeking effective
+                treatments for IBS, Flushed is your trusted companion. Start
+                exploring our website today and take the first step towards
+                reclaiming control over your digestive health and well-being!
               </p>
             </div>
           </div>
-
-          {/* SECTION 2 */}
-          <div id="about" className={styles.section2}>
-            <div className={styles.content2}>
-              <div className={styles.info2}>
-                <div className={styles.txtBox}>
-                  <div className={styles.txtHeads}>
-                    <p className={styles.sec1}>01.</p>
-                    <p className={styles.about}>About Me</p>
-                  </div>
-                  <br />
-                  <br />
-                  <p className={styles.description}>
-                  Discover Effective Treatments for Irritable Bowel Syndrome (IBS) at Flushed!
-
-At Flushed, we understand the significant impact that Irritable Bowel Syndrome (IBS) can have on your daily life. That's why our website is dedicated to providing a comprehensive guide to different types of treatments available to manage IBS symptoms effectively. Whether you're newly diagnosed or have been living with IBS for years, we are here to help you find relief and improve your quality of life.
-
-Our website covers a wide range of treatment options, tailored to address the unique needs and preferences of individuals with IBS. We believe in a holistic approach to IBS management, taking into account the interconnectedness of the mind and body. Here are some of the key treatment strategies we explore:
-
-Dietary Modifications: Flushed delves into the crucial role that diet plays in managing IBS symptoms. We provide in-depth information on various dietary approaches, including the low FODMAP diet, which can help identify and eliminate trigger foods that may exacerbate IBS symptoms. Our articles and resources offer practical guidance and tips for implementing dietary changes effectively.
-
-Lifestyle Adjustments: We recognize that making certain lifestyle adjustments can significantly impact IBS symptoms. Our website offers insights into stress management techniques, regular exercise routines, sleep hygiene practices, and mindfulness exercises that can help alleviate IBS-related stress and promote overall well-being.
-
-Medications: Flushed provides an overview of commonly prescribed medications for IBS, such as antispasmodics, antidiarrheals, and laxatives. We explain how these medications work, their potential side effects, and when they may be recommended as part of an individualized treatment plan. It's important to note that medication should always be discussed with a healthcare professional.
-
-Alternative Therapies: We explore a variety of alternative therapies that have shown promise in managing IBS symptoms. From acupuncture and herbal remedies to probiotics and gut-directed hypnotherapy, Flushed presents evidence-based insights on these complementary approaches, helping you make informed decisions about incorporating them into your treatment regimen.
-
-Psychological Support: Recognizing the strong connection between the gut and the brain, our website emphasizes the importance of psychological support in IBS management. We provide information on cognitive behavioral therapy (CBT), relaxation techniques, and support groups that can help individuals cope with the emotional and psychological aspects of living with IBS.
-
-At Flushed, we strive to empower individuals with knowledge, resources, and support on their journey to finding effective treatment options for IBS. Our website is designed to be a reliable and user-friendly platform, offering a wealth of curated articles, expert advice, and real-life success stories from those who have found relief.
-
-If you're seeking effective treatments for IBS, Flushed is your trusted companion. Start exploring our website today and take the first step towards reclaiming control over your digestive health and well-being!
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+        </div>
+      </div>
 
     </div>
-  
-  )
+  );
 }
