c422af9e670e355bcce022f946ae1aabdaed0e92
Thomas Matlock <thomasmatlockbba@gmail.com>
2023-06-13 10:20:27 -0500

    Add Adsense

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1413499394085097" crossorigin="anonymous"></script>
       <body className={inter.className}>{children}</body>
     </html>
   )
